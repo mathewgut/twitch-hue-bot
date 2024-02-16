02904cda316fab6eb5451918fe6efd72e861834b
Mathew Gutoskie <98121688+mathewgut@users.noreply.github.com>
2024-02-16 12:33:14 -0500

    refactor with commenting
    deleted unused code, added better and more comments, organized code

--- music.py
+++ music.py
@@ -2,192 +2,140 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from passwords import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URL
 from channels_tok import chan_list
-#from twitch_hue
 
+# declare scope for authorization token
 scope = "user-read-playback-state playlist-modify-public playlist-modify-private user-library-read user-library-modify user-top-read"
 
+# authenticating spotify with tokens from accounts, scope, and username of intended account to read/modify
 sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URL, username=chan_list[0]))
 
-
+# returns top played songs from user in specified period
 def liked():
+    # specifying time range and amount of songs to grab from users top played songs
     results = sp.current_user_top_tracks(limit=5, time_range='short_term')
-    scope = 'user-top-read'
-    # Extract the song names and artists
+    # pulling song names and artists
     top_5_songs = [(track['name'] + " by " + track['artists'][0]['name']) for track in results['items']]
     
     return top_5_songs
 
+# legacy function
 def add_to_queue(song):
     sp.add_to_queue(device_id=None)
 
+# return currently playing song on specified account
 def song_current():
-    # Set up authentication
-
-    # Get the current playback information
+    # grabbing information from current playing song on account
     current_playback = sp.current_playback()
 
-    # Check if there is a currently playing track
+    # if there is a currently playing song
     if current_playback is not None and current_playback["is_playing"]:
-        # Get the current track
+        # grab current song
         current_track = current_playback["item"]
-        # Get the track name and artist(s)
+        # save song name and artist
         track_name = current_track["name"]
         artists = [artist["name"] for artist in current_track["artists"]]
+        # return string of currently playing song with artist name
         return f"Currently playing: {track_name} by {', '.join(artists)}"
     else:
+        # if no song planning, return to notify user
         return "No track currently playing"
 
-"""
-queue = {} 
-def request_song(song, author):
-    try:
-        
-        scope = "user-modify-playback-state user-library-read"
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URL, username='itszraven'))
-        results = sp.search(q=song, type="track", limit=1)
-        track_uri = results["tracks"]["items"][0]["uri"]
-
-        # Replace with the URI of the track you want to add
-        queue.update({song:author})
-        print(queue)
-        sp.add_to_queue(track_uri)
-        return(results["tracks"]["items"][0]["name"])
-
-    except Exception as e:
-        return(e)
-"""
+# dictionary of songs as keys, and the requesting user as values
 queue = {}
+# temporary holding for songs to be queued
 stack = []
+# function for adding requested music to queue
 def request_song(song, author, req_stack: list = stack):
     try:
+        # search for song based on request text
         results = sp.search(q=song, type="track", limit=1)
+        # save best match track_uri to variable based on song request text
         track_uri = results["tracks"]["items"][0]["uri"]
         print(track_uri)
+        # rename track_uri variable for legibility
         song = track_uri
+        # add song to request stack
         req_stack.append(song)
+        # updating queue dict with track_uri as key and the requesting user as value
         queue.update({song:author})
+        # if request stack is less than 2, return message to wait for another request
         if len(req_stack) < 2:
             print(req_stack)
             return("waiting")
+        # if request stack is greater or equal to 2
         elif len(req_stack) >= 2:
-            # The ID of the playlist
+            # save queue playlist id
             playlist_id = '3Hlbqq17xTLIGX6Xp0RcVu'
-            # Add the song to the playlist
+            # add songs from stack to queue playlist
             print(req_stack)
             sp.playlist_add_items(playlist_id, req_stack)
             # Update the queue dictionary
             print(queue)
+            # search and save track information based on first position request stack track_uri
             track1 = sp.track(req_stack[0])
+            # save name of first track
             track1_name = track1['name']
+            # save artist of first track
             track1_art = track1['artists'][0]['name']
+            # search and save track information based on second position request stack track_uri
             track2 = sp.track(req_stack[1])
+            # save name of second track name
             track2_name = track2['name']
+            # save second track artist
             track2_art = track2['artists'][0]['name']
             print(req_stack)
+            # clear request stack for next song requests
             req_stack.clear()
+            # return added song information
             return(f"added {track1_name} by {track1_art} and {track2_name} by {track2_art} to queue")
+    # if error, return error message
     except Exception as e:
         return(e)
-
-
-
-#try:
-#print(request('take on me', 'fart'))
-#print(request('dancing queen', 'jeeves'))
-#except Exception as e:
-    #print(f"oops: {e}")
-
-"""
-def remove_song(song, author):
-    scope = "user-modify-playback-state user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URL, username='itszraven'))
-    print(queue[song])
-    if queue[song] == author:
-        queue.pop(song)
-        print(queue)
-        while True:
-            try:
-                sp.next_track()
-                print("fart")
-            except spotipy.exceptions.SpotifyException:
-                print("fart")
-                break
-        print("queue empty")
-        for x in queue:
-            request_song(x, author)
-        return("items requeued")
-    else:
-        
-        return(NameError)
-"""
-
-def remove_song(song, author):
-    # The ID of the playlist
-    playlist_id = '3Hlbqq17xTLIGX6Xp0RcVu'
     
+# function for removing song from playlist 
+def remove_song(song, author):
+    # queue playlist id
+    playlist_id = '3Hlbqq17xTLIGX6Xp0RcVu'
+    # search for song based on request text
     results = sp.search(q=song, type="track", limit=1)
+    # save best match track_uri to variable based on song request text
     track_uri = results["tracks"]["items"][0]["uri"]
 
     print(queue[track_uri])
+    # if requested song removal is from the same person that added the song
     if queue[track_uri] == author:
+        # pop song from queue
         queue.pop(track_uri)
         print(queue)
-        # Remove the song from the playlist
-        ## song is not a uri, fix
+        # save to be removed tracks as list
         track_ids = [track_uri]
+        # remove all instances of song in playlist
         sp.playlist_remove_all_occurrences_of_items(playlist_id, track_ids)
-        
         print("Song removed from playlist")
+        # succuesful status code
         return("removed")
     else:
+        # if not correct user
         return(NameError)
     
-def remove_song(song, author):
-    # The ID of the playlist
-    playlist_id = '3Hlbqq17xTLIGX6Xp0RcVu'
-    
-    results = sp.search(q=song, type="track", limit=1)
-    track_uri = results["tracks"]["items"][0]["uri"]
 
-    print(queue[track_uri])
-    if queue[track_uri] == author:
-        queue.pop(track_uri)
-        print(queue)
-        # Remove the song from the playlist
-        ## song is not a uri, fix
-        track_ids = [track_uri]
-        sp.playlist_remove_all_occurrences_of_items(playlist_id, track_ids)
-        
-        print("Song removed from playlist")
-        return("removed")
-    else:
-        return(NameError)
-    
 def get_tracks():
+    # queue playlist id
     playlist_id = '3Hlbqq17xTLIGX6Xp0RcVu'
+    # temporary list for storing song information
     tracks = []
+    # for song in queue playlist
     for item in sp.playlist_tracks(playlist_id)["items"]:
+        # save current song
         track = item["track"]
+        # save current track uri
         track_uri = track["uri"]
+        # save current song name
         track_name = track["name"]
-        # Get the name of the first artist
+        # save name of artist
         track_artist = track["artists"][0]["name"]
+        # add track name and artist to tracks list
         tracks.append(f"{track_name} - {track_artist}")
     print(tracks)
+    # return list of all songs currently in queue
     return tracks
-
-
-def clear_queue():
-    scope = "user-modify-playback-state user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URL, username='itszraven'))
-    while True:
-        try:
-            sp.next_track()
-            print("fart")
-        except spotipy.exceptions.SpotifyException:
-            print("fart")
-            break
-    print(queue)
-    return("queue empty")
-
-##print(remove_song('dancing queen', 'jeeves'))
